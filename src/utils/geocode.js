46a31395392534cea2a3df8d5d21e05810517e6f
KatV <katv220@gmail.com>
2019-10-14 15:01:11 +0000

    Add about text

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibGlzYW5kaXIiLCJhIjoiY2swcHFqYWZpMDBsNjNubzg2cnNiMzQ4ayJ9.C0QZ9D-CMGhAg--wJyivLg'
-    
+
     request({url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services', undefined)
